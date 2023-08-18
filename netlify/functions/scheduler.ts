51474363197b7097135577c152898138a0f41238
Steven Ledsworth <sledsworth@gmail.com>
2023-08-18 14:56:13 -0400

    cron on odd hours of day

--- netlify/functions/scheduler.ts
+++ netlify/functions/scheduler.ts
@@ -71,6 +71,6 @@ const myHandler: Handler = async (
   }
 }
 
-const handler = schedule('0 */2 * * *', myHandler)
+const handler = schedule('0 1,3,5,7,9,11,13,15,17,19,21,23 * * *', myHandler)
 
 export { handler }
